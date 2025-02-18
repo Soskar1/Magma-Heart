9ce8f0b40f0a58bc8c726ca34c3f19c0ac557d16
Soskar1 <oskar.vistorskis@gmail.com>
2025-02-18 16:45:22 +0200

    Added RigidbodyMovememnt

--- Assets/_Scripts/Core/Player.cs
+++ Assets/_Scripts/Core/Player.cs
@@ -2,24 +2,23 @@ using UnityEngine;
 
 namespace MagmaHeart.Core
 {
-    [RequireComponent(typeof(Rigidbody2D))]
+    [RequireComponent(typeof(RigidbodyMovement))]
     public class Player : MonoBehaviour
     {
         private UserInput m_userInput;
-        private Rigidbody2D m_rb2d;
-        [SerializeField] private float m_speed;
+        private RigidbodyMovement m_movement;
 
         private void Awake() 
         {
             m_userInput = new UserInput();
-            m_rb2d = GetComponent<Rigidbody2D>();
+            m_movement = GetComponent<RigidbodyMovement>();
         }
 
         private void Start() => m_userInput.Enable();
         
         public void FixedUpdate()
         {
-            m_rb2d.AddForce(m_userInput.Movement * m_speed);
+            m_movement.Move(m_userInput.Movement);
         }                
     }
 }
---
+++ Assets/_Scripts/Core/RigidbodyMovement.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+namespace MagmaHeart.Core
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class RigidbodyMovement : MonoBehaviour
+    {
+        [SerializeField] private float m_maxSpeed;
+        [SerializeField] private float m_acceleration;
+        [SerializeField] private float m_decceleration;
+        [SerializeField] private float m_velocityPower;
+        private Rigidbody2D m_rigidbody;
+
+        private const float EPSILON = 0.01f;
+
+        private void Awake() => m_rigidbody = GetComponent<Rigidbody2D>();
+
+        public void Move(Vector2 direction)
+        {
+            Vector2 targetVelocity = direction * m_maxSpeed;
+            Vector2 velocityDifference = targetVelocity - m_rigidbody.linearVelocity;
+            float accelerationRate = targetVelocity.magnitude > EPSILON ? m_acceleration : m_decceleration;
+
+            float movementX = Mathf.Pow(Mathf.Abs(velocityDifference.x) * accelerationRate, m_velocityPower) * Mathf.Sign(velocityDifference.x);
+            float movementY = Mathf.Pow(Mathf.Abs(velocityDifference.y) * accelerationRate, m_velocityPower) * Mathf.Sign(velocityDifference.y);
+
+            m_rigidbody.AddForce(new Vector2(movementX, movementY));
+        }
+    }
+}
