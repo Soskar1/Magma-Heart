c623549a923d292fe935dd3ae385a392cabbacfb
Oskaras Vištorskis <89640069+Soskar1@users.noreply.github.com>
2025-03-27 19:18:34 +0200

    Controlling player animations via code

--- Assets/_Scripts/Core/Entities/Player.cs
+++ Assets/_Scripts/Core/Entities/Player.cs
@@ -12,11 +12,20 @@ namespace MagmaHeart.Core.Entities
         private Animator m_animator;
         [SerializeField] private bool m_facingRight;
 
+        private bool m_isAttacking = false;
+
+        private readonly int m_idleAnimationID = Animator.StringToHash("Idle");
+        private readonly int m_runAnimationID = Animator.StringToHash("Run");
+        private readonly int m_attackAnimationID = Animator.StringToHash("Attack");
+        private int m_currentAnimationState;
+
         private void Awake() 
         {
             m_userInput = new UserInput();
             m_movement = GetComponent<RigidbodyMovement>();
             m_animator = GetComponent<Animator>();
+
+            m_currentAnimationState = m_idleAnimationID;
         }
 
         private void Start()
@@ -33,16 +42,38 @@ namespace MagmaHeart.Core.Entities
 
         public void Update()
         {
-            if (Mathf.Abs(m_userInput.Movement.magnitude) > 0)
-                m_animator.SetBool("IsRunning", true);
-            else
-                m_animator.SetBool("IsRunning", false);
-            
             if ((m_userInput.Movement.x > 0 && !m_facingRight) || (m_userInput.Movement.x < 0 && m_facingRight))
             {
                 transform.Rotate(new Vector3(0, 180, 0));
                 m_facingRight = !m_facingRight;
             }
+
+            PlayAnimations();
+        }
+
+        private void PlayAnimations()
+        {
+            int stateToPlay = GetAnimationState();
+            
+            if (stateToPlay == m_currentAnimationState)
+                return;
+            
+            m_animator.CrossFade(stateToPlay, 0);
+            m_currentAnimationState = stateToPlay;
+        }
+
+        private int GetAnimationState()
+        {
+            if (m_animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && m_currentAnimationState == m_attackAnimationID)
+                m_isAttacking = false;
+
+            if (m_isAttacking)
+                return m_attackAnimationID;
+            
+            if (Mathf.Abs(m_userInput.Movement.magnitude) > 0)
+                return m_runAnimationID;
+            
+            return m_idleAnimationID;
         }
 
         public void FixedUpdate()
@@ -52,7 +83,7 @@ namespace MagmaHeart.Core.Entities
 
         public void Attack(InputAction.CallbackContext context)
         {
-            m_animator.SetTrigger("Attack");
+            m_isAttacking = true;
         }
     }
 }
