c6091b7b477e166773d12e9599978f08687d82d6
oskar <oskar.vistorskis@gmail.com>
2025-11-05 10:34:47 +0200

    Added property snapshots

---
+++ Assets/_Scripts/Core/Entities/Properties/EnergyProperty.cs
@@ -0,0 +1,6 @@
+﻿using MagmaHeart.AI.Reasoning;
+
+namespace MagmaHeart.Core.Entities.Properties
+{
+    public record EnergyProperty(float CurrentEnergy) : PropertySnapshot;
+}
---
+++ Assets/_Scripts/Core/Entities/Properties/HealthProperty.cs
@@ -0,0 +1,6 @@
+﻿using MagmaHeart.AI.Reasoning;
+
+namespace MagmaHeart.Core.Entities.Properties
+{
+    public record HealthProperty(float CurrentHealth, float MaxHealth) : PropertySnapshot;
+}
