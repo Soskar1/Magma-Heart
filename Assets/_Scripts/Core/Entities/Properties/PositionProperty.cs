9521184dee8f7d3c5ca3b3059ca48798de4ad92c
oskar <oskar.vistorskis@gmail.com>
2025-11-05 16:09:23 +0200

    Added ManhattanDistance calculation

--- Assets/_Scripts/Core/Entities/Properties/PositionProperty.cs
+++ Assets/_Scripts/Core/Entities/Properties/PositionProperty.cs
@@ -1,7 +1,11 @@
 ﻿using MagmaHeart.AI.Reasoning;
+using MagmaHeart.Core.Dungeon;
 using UnityEngine;
 
 namespace MagmaHeart.Core.Entities.Properties
 {
-    public record PositionProperty(Vector3Int position) : PropertySnapshot;
+    public record PositionProperty(Vector3Int Position) : PropertySnapshot
+    {
+        public int ManhattanDistance(PositionProperty other) => DungeonGrid.ManhattanDistance(Position, other.Position);   
+    }
 }
