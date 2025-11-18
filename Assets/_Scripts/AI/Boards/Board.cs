7309d5e928c29a88cd0f4da3dc6b201db4274f45
oskar <oskar.vistorskis@gmail.com>
2025-11-18 17:14:09 +0200

    Added operations with unit dictionary in board. Added tests

--- Assets/_Scripts/AI/Boards/Board.cs
+++ Assets/_Scripts/AI/Boards/Board.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace MagmaHeart.AI.Boards
@@ -6,21 +7,44 @@ namespace MagmaHeart.AI.Boards
     public class Board
     {
         public BoardGraph Graph { get; init; }
-        public Dictionary<Vector2, AIUnit> Units { get; init; }
+
+        private Dictionary<Vector2, AIUnit> m_units;
+        public IEnumerable<AIUnit> Units => m_units.Values;
 
         public Board(BoardGraph graph)
         {
             Graph = graph;
-            Units = new Dictionary<Vector2, AIUnit>();
+            m_units = new Dictionary<Vector2, AIUnit>();
         }
 
         public void ChangeNodeType(Vector2 position, BoardNodeType newNodeType) => Graph.ChangeNodeType(position, newNodeType);
+
+        public void AddUnit(Vector2 position, AIUnit unit)
+        {
+            if (Graph.GetNode(position) == null)
+                throw new ArgumentException($"Node at {position} does not exist in the board graph!");
+
+            if (m_units.ContainsKey(position))
+            {
+                Debug.LogWarning($"There is already a unit at position {position}!");
+                return;
+            }
+
+            m_units.Add(position, unit);
+        }
+
+        public bool RemoveUnit(Vector2 position) => m_units.Remove(position);
+
+        public bool TryGetUnit(Vector2 position, out AIUnit unit) => m_units.TryGetValue(position, out unit);
 
         public Board DeepCopy()
         {
             BoardGraph graph = Graph.DeepCopy();
             Board board = new Board(graph);
-            // TODO
+            
+            foreach (var keyValuePair in m_units)
+                board.AddUnit(keyValuePair.Key, keyValuePair.Value);
+
             return board;
         }
     }
---
+++ Assets/_Scripts/AI/Tests/Boards/BoardTests.cs
@@ -0,0 +1,114 @@
+﻿using MagmaHeart.Collections;
+using NUnit.Framework;
+using System;
+using UnityEngine;
+
+namespace MagmaHeart.AI.Boards.Tests
+{
+    public class BoardTests
+    {
+        private Board m_board;
+        private AIUnit Unit => new AIUnit(false, new TypeMap<Actions.Action>());
+
+        [SetUp]
+        public void SetUp()
+        {
+            BoardGraph graph = new BoardGraph();
+            graph.AddNode(Vector2.zero, BoardNodeType.Walkable);
+            graph.AddNode(Vector2.up, BoardNodeType.Walkable);
+            graph.AddNode(Vector2.right, BoardNodeType.Obstacle);
+            graph.AddNode(Vector2.left, BoardNodeType.Obstacle);
+
+            m_board = new Board(graph);
+        }
+
+        [Test]
+        public void AddUnit_ExistingNode_AddsUnit()
+        {
+            AIUnit unit = Unit;
+
+            m_board.AddUnit(Vector2.zero, unit);
+
+            Assert.That(m_board.TryGetUnit(Vector2.zero, out AIUnit unitOnBoard), Is.True);
+            Assert.That(unitOnBoard, Is.EqualTo(unit));
+        }
+
+        [Test]
+        public void AddUnit_NonExistingNode_ThrowsException()
+        {
+            AIUnit unit = Unit;
+
+            Assert.Throws<ArgumentException>(() => m_board.AddUnit(new Vector2(1, 1), unit));
+        }
+
+        [Test]
+        public void AddUnit_MultipleUnitsOnOneNode_DoesNotUpdateUnitOnTheBoard()
+        {
+            AIUnit unit1 = Unit;
+            AIUnit unit2 = Unit;
+            m_board.AddUnit(Vector2.zero, unit1);
+
+            m_board.AddUnit(Vector2.zero, unit2);
+
+            Assert.That(m_board.TryGetUnit(Vector2.zero, out AIUnit unitOnBoard), Is.True);
+            Assert.That(unitOnBoard, Is.EqualTo(unit1));
+        }
+
+        [Test]
+        public void RemoveUnit_ExistingUnit_RemovesUnit()
+        {
+            AIUnit unit = Unit;
+            m_board.AddUnit(Vector2.zero, unit);
+
+            bool result = m_board.RemoveUnit(Vector2.zero);
+
+            Assert.That(result, Is.True);
+            Assert.That(m_board.TryGetUnit(Vector2.zero, out _), Is.False);
+        }
+
+        [Test]
+        public void RemoveUnit_NonExistingUnit_DoNothing()
+        {
+            bool result = m_board.RemoveUnit(Vector2.zero);
+
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void TryGetUnit_ExistingUnit_ReturnsTrueAndUnit()
+        {
+            AIUnit unit = Unit;
+            m_board.AddUnit(Vector2.zero, unit);
+
+            bool result = m_board.TryGetUnit(Vector2.zero, out AIUnit unitOnBoard);
+
+            Assert.That(result, Is.True);
+            Assert.That(unitOnBoard, Is.EqualTo(unit));
+        }
+
+        [Test]
+        public void TryGetUnit_NonExistingUnit_ReturnsFalseAndNullUnit()
+        {
+            bool result = m_board.TryGetUnit(Vector2.zero, out AIUnit unit);
+
+            Assert.That(result, Is.False);
+            Assert.That(unit, Is.Null);
+        }
+
+        [Test]
+        public void DeepCopy_ReturnsBoardCopy()
+        {
+            AIUnit unit1 = Unit;
+            AIUnit unit2 = Unit;
+            m_board.AddUnit(Vector2.zero, unit1);
+            m_board.AddUnit(Vector2.up, unit2);
+
+            Board copy = m_board.DeepCopy();
+
+            Assert.That(ReferenceEquals(m_board, copy), Is.False);
+            Assert.That(copy.TryGetUnit(Vector2.zero, out AIUnit unitCopy1), Is.True);
+            Assert.That(copy.TryGetUnit(Vector2.up, out AIUnit unitCopy2), Is.True);
+            Assert.That(ReferenceEquals(copy.Graph, m_board.Graph), Is.False);
+        }
+    }
+}
