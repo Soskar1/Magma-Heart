08b3180a4cfcb82ec3e6bb0b4fd2f32cee83e309
oskar <oskar.vistorskis@gmail.com>
2025-11-23 17:16:40 +0200

    Fixed CurrentEnergy setter in EnergyPropertySnapshot

--- Assets/_Scripts/Core/Entities/Properties/EnergyPropertySnapshot.cs
+++ Assets/_Scripts/Core/Entities/Properties/EnergyPropertySnapshot.cs
@@ -1,6 +1,17 @@
 ﻿using MagmaHeart.AI.States;
+using System;
 
 namespace MagmaHeart.Core.Entities.Properties
 {
-    public record EnergyPropertySnapshot(int CurrentEnergy) : PropertySnapshot;
+    public record EnergyPropertySnapshot : PropertySnapshot
+    {
+        public int CurrentEnergy { get; init; }
+        public int MaxEnergy { get; init; }
+
+        public EnergyPropertySnapshot(int currentEnergy, int maxEnergy)
+        {
+            MaxEnergy = maxEnergy;
+            CurrentEnergy = Math.Clamp(currentEnergy, 0, MaxEnergy);
+        }
+    }
 }
