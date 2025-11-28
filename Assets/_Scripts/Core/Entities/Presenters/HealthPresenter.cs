a04313bad4b3a02bab5039bb597ec8ac2a5c52c1
oskar <oskar.vistorskis@gmail.com>
2025-11-28 17:56:23 +0200

    Moved health bar to the bottom center of the screen

--- Assets/_Scripts/Core/Entities/Presenters/HealthPresenter.cs
+++ Assets/_Scripts/Core/Entities/Presenters/HealthPresenter.cs
@@ -1,5 +1,6 @@
 using MagmaHeart.Core.Entities.Models;
 using MagmaHeart.Core.Entities.PlayableCharacters;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,22 +9,25 @@ namespace MagmaHeart.Core.Entities.Presenters
     public class HealthPresenter : MonoBehaviour
     {
         [SerializeField] private Image m_healthBar;
+        [SerializeField] private TextMeshProUGUI m_text;
         private HealthModel m_healthModel;
 
         public void Initialize(Player player)
         {
             m_healthModel = player.Health;
-            m_healthModel.OnHealthChanged += UpdateHealthBar;
+            m_healthModel.OnHealthChanged += HandleOnHealthChangedEvent;
+
+            UpdateHealthBar(m_healthModel.CurrentHealth, m_healthModel.MaxHealth);
         }
 
-        private void OnDisable()
-        {
-            m_healthModel.OnHealthChanged -= UpdateHealthBar;
-        }
+        private void OnDisable() => m_healthModel.OnHealthChanged -= HandleOnHealthChangedEvent;
 
-        public void UpdateHealthBar(object obj, OnHealthChangedEventArgs e)
+        private void HandleOnHealthChangedEvent(object obj, OnHealthChangedEventArgs e) => UpdateHealthBar(e.CurrentHealth, e.MaxHealth);
+
+        private void UpdateHealthBar(float currentHealth, float maxHealth)
         {
-            m_healthBar.fillAmount = e.CurrentHealth / e.MaxHealth;
+            m_healthBar.fillAmount = currentHealth / maxHealth;
+            m_text.text = $"{currentHealth}/{maxHealth}";
         }
     }
 }
