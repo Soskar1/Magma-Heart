d3c861b51a8baa2fa2a482aab16c74a25ff9ece1
oskar <oskar.vistorskis@gmail.com>
2025-12-06 15:29:02 +0200

    cleanup

--- Assets/_Scripts/Core/Entities/EntityAnimation.cs
+++ Assets/_Scripts/Core/Entities/EntityAnimation.cs
@@ -10,8 +10,6 @@ namespace MagmaHeart.Core.Entities
         private readonly int m_runAnimationID = Animator.StringToHash("Run");
         private readonly int m_attackAnimationID = Animator.StringToHash("Attack");
 
-        public event EventHandler OnAttackAnimationEnded;
-
         private TaskCompletionSource<bool> m_animationTrigger;
 
         public void PlayIdleAnimation() => CurrentAnimationState = m_idleAnimationID;
@@ -30,11 +28,5 @@ namespace MagmaHeart.Core.Entities
         }
 
         public void AnimationTrigger() => m_animationTrigger.SetResult(true);
-
-        //public override void FireOnAnimationEndedEvent()
-        //{
-        //    if (CurrentAnimationState == m_attackAnimationID)
-        //        OnAttackAnimationEnded?.Invoke(this, EventArgs.Empty);
-        //}  
     }
 }
