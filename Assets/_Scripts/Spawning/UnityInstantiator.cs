4ba3f0e94db735e4cf4be71f79c0e9a6e22ad4fc
Soskar1 <oskar.vistorskis@gmail.com>
2026-01-04 14:43:47 +0200

    Removed MagmaHeart.Spawning assembly

--- Assets/_Scripts/Spawning/UnityInstantiator.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace MagmaHeart.Spawning
-{
-    public class UnityInstantiator : IInstantiator
-    {
-        public GameObject Instantiate(GameObject prefab, Vector2 position) => Object.Instantiate(prefab, position, Quaternion.identity);
-    }
-}
