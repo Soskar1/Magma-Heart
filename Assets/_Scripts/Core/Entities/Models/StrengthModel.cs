3f73730416555c7b08b4af3f0dc511788a29ca59
oskar <oskar.vistorskis@gmail.com>
2026-01-05 18:30:46 +0200

    Added strength stat

---
+++ Assets/_Scripts/Core/Entities/Models/StrengthModel.cs
@@ -0,0 +1,15 @@
+﻿namespace MagmaHeart.Core.Entities.Models
+{
+    public class StrengthModel
+    {
+        private int m_currentStrength;
+
+        public int CurrentStrength
+        {
+            get => m_currentStrength;
+            set => m_currentStrength = value;
+        }
+
+        public StrengthModel(int initialStrength) => m_currentStrength = initialStrength;
+    }
+}
