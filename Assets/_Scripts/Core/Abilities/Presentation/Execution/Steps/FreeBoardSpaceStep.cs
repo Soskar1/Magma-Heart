9df9c4a5d74cadef0d08f485ae76f965abbd7bc7
oskar <oskar.vistorskis@gmail.com>
2026-02-25 17:29:14 +0200

    Added more steps

---
+++ Assets/_Scripts/Core/Abilities/Presentation/Execution/Steps/FreeBoardSpaceStep.cs
@@ -0,0 +1,43 @@
+﻿using MagmaHeart.AI.Boards;
+using MagmaHeart.Core.Abilities.Effects;
+using MagmaHeart.Extensions;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace MagmaHeart.Core.Abilities.Presentation.Execution.Steps
+{
+    [Serializable]
+    public class FreeBoardSpaceStep : IAbilityExecutionStep
+    {
+        public Task Run(AbilityExecutionContext context, CancellationToken cancellationToken)
+        {
+            if (cancellationToken.IsCancellationRequested)
+                return Task.FromCanceled(cancellationToken);
+
+            MoveEffect moveEffect = context.Plan.Effects.OfType<MoveEffect>().FirstOrDefault();
+
+            if (moveEffect == null)
+            {
+                Debug.LogError($"{nameof(FreeBoardSpaceStep)} requires a MoveEffect in the plan to determine which space to free.");
+                return Task.CompletedTask;
+            }
+
+            Vector3 start = moveEffect.Path.First();
+            Vector2 startTile = context.World.WorldToTilePosition(start).ToVector2();
+
+            bool isUnitRemoved = context.World.CurrentRoom.RemoveUnit(startTile);
+
+            if (!isUnitRemoved)
+            {
+                Debug.LogError($"{nameof(FreeBoardSpaceStep)} failed to remove unit from the board at position {startTile}");
+                return Task.CompletedTask;
+            }
+
+            context.World.CurrentRoom.ChangeNodeType(startTile, BoardNodeType.Walkable);
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Assets/_Scripts/Core/Abilities/Presentation/Execution/Steps/OccupyBoardSpaceStep.cs
@@ -0,0 +1,45 @@
+﻿using MagmaHeart.AI.Boards;
+using MagmaHeart.Core.Abilities.Effects;
+using MagmaHeart.Core.Entities;
+using MagmaHeart.Extensions;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace MagmaHeart.Core.Abilities.Presentation.Execution.Steps
+{
+    [Serializable]
+    public class OccupyBoardSpaceStep : IAbilityExecutionStep
+    {
+        public Task Run(AbilityExecutionContext context, CancellationToken cancellationToken)
+        {
+            if (cancellationToken.IsCancellationRequested)
+                return Task.FromCanceled(cancellationToken);
+
+            MoveEffect moveEffect = context.Plan.Effects.OfType<MoveEffect>().FirstOrDefault();
+
+            if (moveEffect == null)
+            {
+                Debug.LogWarning($"{nameof(FreeBoardSpaceStep)} requires a MoveEffect in the plan to determine which space to free.");
+                return Task.CompletedTask;
+            }
+
+            context.World.TryGetEntity(context.ExecutorId, out Entity executor);
+
+            if (executor == null)
+            {
+                Debug.LogWarning($"{nameof(FreeBoardSpaceStep)}: entity with id '{context.ExecutorId}' was not found!");
+                return Task.CompletedTask; 
+            }
+
+            Vector3 end = moveEffect.Path.Last();
+            Vector2 endTile = context.World.WorldToTilePosition(end).ToVector2();
+
+            context.World.CurrentRoom.AddUnit(endTile, executor.Model);
+            context.World.CurrentRoom.ChangeNodeType(endTile, BoardNodeType.Obstacle);
+            return Task.CompletedTask;
+        }
+    }
+}
