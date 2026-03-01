592343858c9eeb611e2663855f41cd571edac8f7
Soskar1 <oskar.vistorskis@gmail.com>
2026-03-01 13:46:30 +0200

    Optimized execution scripts

--- Assets/_Scripts/Core/Abilities/Presentation/Execution/Steps/MoveEntityStep.cs
+++ Assets/_Scripts/Core/Abilities/Presentation/Execution/Steps/MoveEntityStep.cs
@@ -1,11 +1,11 @@
-﻿using MagmaHeart.Core.Abilities.Effects;
-using MagmaHeart.Core.Entities;
-using MagmaHeart.Extensions;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using MagmaHeart.AI.Boards;
+using MagmaHeart.Core.Abilities.Effects;
+using MagmaHeart.Core.Entities;
 using UnityEngine;
 
 namespace MagmaHeart.Core.Abilities.Presentation.Execution.Steps
@@ -20,6 +20,12 @@ namespace MagmaHeart.Core.Abilities.Presentation.Execution.Steps
 
             context.World.TryGetEntity(context.ExecutorId, out Entity entity);
 
+            if (entity == null)
+            {
+                Debug.LogWarning($"{nameof(MoveEntityStep)}: entity with id '{context.ExecutorId}' was not found!");
+                return;
+            }
+
             MoveEffect moveEffect = context.Plan.Effects
                 .OfType<MoveEffect>()
                 .FirstOrDefault();
@@ -31,9 +37,34 @@ namespace MagmaHeart.Core.Abilities.Presentation.Execution.Steps
             if (entity.Model.TilePosition == tiledPath.Last().ToVector3Int())
                 return;
 
-            entity.Facing.TryUpdateFacing(tiledPath.Last().x - entity.Model.TilePosition.x);
+            RemoveUnitFromBoard(context.World, moveEffect.Path.First());
 
+            entity.Facing.TryUpdateFacing(tiledPath.Last().x - entity.Model.TilePosition.x);
             await entity.TileBasedMovement.StartMovementAsync(tiledPath);
+
+            AddUnitToBoard(context.World, moveEffect.Path.Last(), entity.Model);
+        }
+
+        private void RemoveUnitFromBoard(GameWorld world, Vector3 start)
+        {
+            Vector2 startTile = world.WorldToTilePosition(start);
+
+            bool isUnitRemoved = world.RemoveUnit(startTile);
+
+            if (!isUnitRemoved)
+            {
+                Debug.LogError($"{nameof(MoveEntityStep)} failed to remove unit from the board at position {startTile}");
+                return;
+            }
+
+            world.ChangeNodeType(startTile, BoardNodeType.Walkable);
+        }
+
+        private void AddUnitToBoard(GameWorld world, Vector3 end, EntityModel entity)
+        {
+            Vector2 endTile = world.WorldToTilePosition(end);
+            world.AddUnit(endTile, entity);
+            world.ChangeNodeType(endTile, BoardNodeType.Obstacle);
         }
     }
 }
