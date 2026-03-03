4fd2c1992966082876312cd49a0ceffbc330eeee
oskar <oskar.vistorskis@gmail.com>
2026-03-03 19:17:40 +0200

    Added bresenham line of sight requirement for the ranged attack

--- Assets/_Scripts/Abilities/Requirements/IAbilityRequirement.cs
+++ Assets/_Scripts/Abilities/Requirements/IAbilityRequirement.cs
@@ -6,4 +6,11 @@ namespace MagmaHeart.Abilities.Requirements
     {
         public abstract bool IsMet(IGameWorld gameWorld, int executorId, AbilityTarget target);
     }
+
+    public interface IAbilityRequirement<T> : IAbilityRequirement where T : IGameWorld
+    {
+        public abstract bool IsMet(T gameWorld, int executorId, AbilityTarget target);
+
+        bool IAbilityRequirement.IsMet(IGameWorld gameWorld, int executorId, AbilityTarget target) => IsMet((T)gameWorld, executorId, target);
+    }
 }
---
+++ Assets/_Scripts/Core/Abilities/Requirements/EnemyIsInLineOfSight.cs
@@ -0,0 +1,38 @@
+﻿using MagmaHeart.Abilities.Requirements;
+using MagmaHeart.Abilities.Targeting;
+using MagmaHeart.AI;
+using MagmaHeart.AI.Boards;
+using MagmaHeart.Bresenham;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MagmaHeart.Core.Abilities.Requirements
+{
+    [System.Serializable]
+    public class EnemyIsInLineOfSight : IAbilityRequirement<IBoardGameWorld>
+    {
+        public bool IsMet(IBoardGameWorld world, int executorId, AbilityTarget target)
+        {
+            bool isTargetingEntity = target.Kind.HasFlag(TargetKind.Entity);
+
+            if (!isTargetingEntity)
+                return false;
+
+            Vector2Int executorPosition = world.GetEntityPosition(executorId).ToVector2Int();
+            Vector2Int targetPosition = world.GetEntityPosition(target.EntityId).ToVector2Int();
+
+            IEnumerable<Vector2Int> tiles = BresenhamLine.DrawLine(executorPosition, targetPosition);
+
+            foreach (Vector2Int tile in tiles)
+            {
+                BoardNodeType tileType = world.GetNodeType(tile.ToVector2());
+                bool isObstacle = tileType == BoardNodeType.Obstacle;
+
+                if (isObstacle && tile != executorPosition && tile != targetPosition)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
