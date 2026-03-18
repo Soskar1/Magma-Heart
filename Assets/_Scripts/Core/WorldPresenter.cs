dd5555e03feec57d4ccb8514398d70a30f1add4e
oskar <oskar.vistorskis@gmail.com>
2026-03-18 18:13:19 +0200

    Added decorations

--- Assets/_Scripts/Core/WorldPresenter.cs
+++ Assets/_Scripts/Core/WorldPresenter.cs
@@ -1,6 +1,7 @@
 using MagmaHeart.Core.Dungeon;
 using MagmaHeart.DungeonGeneration;
 using System.Collections;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.Tilemaps;
@@ -10,10 +11,15 @@ namespace MagmaHeart.Core
     public class WorldPresenter : MonoBehaviour
     {
         [SerializeField] private Tilemap m_tilemap;
+        [SerializeField] private Tilemap m_decorations;
         [SerializeField] private int m_tilesPerFrame = 256;
         [SerializeField] private TileBase m_floor;
         [SerializeField] private TileBase m_wall;
         [SerializeField] private TileBase m_door;
+
+        [SerializeField] private float m_decorationChance = 0.1f;
+
+        [SerializeField] private List<TileBase> m_decorationTiles;
 
         private GameWorld m_gameWorld;
 
@@ -44,11 +50,23 @@ namespace MagmaHeart.Core
                 Vector3Int tilePosition = m_tilemap.WorldToCell((Vector3Int)tile.Position);
 
                 if (tile == roomModel.EntranceDoor || tile == roomModel.ExitDoor)
+                {
                     m_tilemap.SetTile(tilePosition, m_door);
+                }
                 else if (tile.Type == TileType.Floor)
+                {
                     m_tilemap.SetTile(tilePosition, m_floor);
+
+                    if (Random.value < m_decorationChance)
+                    {
+                        TileBase decorationTile = m_decorationTiles[Random.Range(0, m_decorationTiles.Count)];
+                        m_decorations.SetTile(tilePosition, decorationTile);
+                    }
+                }
                 else
+                {
                     m_tilemap.SetTile(tilePosition, m_wall);
+                }
 
                 ++renderedTiles;
 
@@ -59,6 +77,10 @@ namespace MagmaHeart.Core
             m_renderedRoom.SetResult(true);
         }
 
-        public void Clear() => m_tilemap.ClearAllTiles();
+        public void Clear()
+        {
+            m_decorations.ClearAllTiles();
+            m_tilemap.ClearAllTiles();
+        }
     }
 }
